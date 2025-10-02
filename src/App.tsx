bed3c503d357bbbf75717a455a909a5f9ab9d066
jose100200 <josec.paradah@gmail.com>
2025-10-02 15:48:32 +0200

    Add class names to card component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,30 @@ import Card from './components/Card.tsx'
 function App() {
 
   return (
-    <div className='page'>
-      <Card title="BTC Dominance">
-        <p>This is where the BTC chart will go later.</p>
-      </Card>
+    <body>
+      <div className='page'>
 
-      <Card title="Fear & Greed">
-        <p>Here we’ll put the gauge chart.</p>
-      </Card>
+        <div className='btc-card'>
+          <Card title="BTC Dominance">
+          <p>This is where the BTC chart will go later.</p>
+        </Card>
+        </div>
 
-      <Card title="TOTAL3ES / ETH">
-        <p>Here we'll put the ratio number.</p>
-      </Card>
+        <div className='others-card'>
+          <div>
+            <Card title="Fear & Greed">
+              <p>Here we’ll put the gauge chart.</p>
+            </Card>
+          </div>  
+          <div>
+            <Card title="TOTAL3ES / ETH">
+              <p>Here we'll put the ratio number.</p>
+            </Card>
+          </div>
+        </div>
 
-    </div>
+      </div>
+    </body>
       
   );
 };
